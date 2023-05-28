ff0400d4fae405d7360070fabf3f6cb7220c14a3
Kim Gimun <kmkim7575@gmail.com>
2023-05-28 23:04:37 +0900

    [FEAT] Rows and Columns

--- app/src/main/java/com/practice/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/com/practice/jetpack_compose_practice/MainActivity.kt
@@ -3,41 +3,37 @@ package com.practice.jetpack_compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.practice.jetpack_compose_practice.ui.theme.JetpackcomposepracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackcomposepracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Column(
+                modifier = Modifier
+//                    .fillMaxSize()
+//                    .fillMaxHeight(0.7f)
+//                    .fillMaxWidth(0.5f)
+                    .width(100.dp)
+                    .height(100.dp)
+                    .background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text("Hello")
+                Text("World")
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackcomposepracticeTheme {
-        Greeting("Android")
     }
 }
