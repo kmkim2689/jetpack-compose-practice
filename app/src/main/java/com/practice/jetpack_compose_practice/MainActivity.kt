4f69851cd9582df4dc7cc06b744245be8657f529
Kim Gimun <kmkim7575@gmail.com>
2023-05-29 02:20:04 +0900

    [FEAT] styling text

--- app/src/main/java/com/practice/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/com/practice/jetpack_compose_practice/MainActivity.kt
@@ -23,7 +23,16 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,82 +42,77 @@ import com.practice.jetpack_compose_practice.ui.theme.Shapes
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+//        상황에 따라 다른 종류의 font를 사용하기 위하여 FontFamily를 이용
+        val fontFamily = FontFamily(
+//            fontweight가 thin으로 설정되었을 때 사용할 폰트
+            Font(R.font.playfair_italic_variablefont, FontWeight.Thin),
+            Font(R.font.playfair_variablefont, FontWeight.Light),
+//            ctrl+d를 하면 해당 코드를 붙여넣을 수 있음
+        )
+
         setContent {
-//            jetpack compose에서 image resource를 사용하려면, painterResource라는 메소드를 이용
-            val painter = painterResource(id = R.drawable.sharehands_logo)
-            val description = "it is the logo"
-            val title = "sharehands logo"
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f)) {
-                ImageCard(painter = painter, contentDescription = description, title = title)
-            }
-
-        }
-    }
-}
-
-// 재사용 가능한 Composable을 제작
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(
-            modifier = Modifier
-                .height(200.dp)
-        ) {
-//            여기에 다른 composable들을 정의하면, 위에 겹쳐 쌓이게 된다.
-//            즉 가장 먼저 쓰이는 것이 가장 밑으로 가게 되는 것.
-//            이미지(Image)부터 작성하고, 그 다음에 글을 작성해야 할것.
-            Image(painter = painter, 
-                contentDescription = contentDescription,
-//                ContentScale.Crop : centerCrop와 유사
-            contentScale = ContentScale.Crop)
-
-//            이미지 위에 gradient를 주기 위하여, 새로운 box를 또 만들어 거기에 gradient 적용
-            Box(modifier = Modifier.fillMaxSize()
-//                    음영을 주려면, background에 Brush를 두면 된다.
-                .background(Brush.verticalGradient(
-                    colors = listOf(
-                        Color.Transparent, Color.Black
-                    ),
-                    startY = 300f
-                ))) {
-
-            }
-            
-//            여기에 바로 Text를 정의하면, 정렬하기가 힘들다. 따라서 Box를 다시 한 번 정의하고
-//            그 Box를 정렬하는 것이 좋음
-//            fillmaxsize를 통하여 부모 box를 모두 채우도록 함
-//            contentAlignment를 이용하여 box가 부모 요소에서 어디 위치에 있을 것인지 결정
             Box(modifier = Modifier
                 .fillMaxSize()
-                .padding(12.dp),
-            contentAlignment = Alignment.BottomStart) {
-                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                .background(Color(0xFF101010))) {
+                Text(
+//                    annotatedString을 통하여, 텍스트의 일부만 스타일링 가능
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+//                            withStyle에서 정의한 스타일을 적용하기 위하여,
+//                            withStyle 내부의 블록에서 append를 통하여 해당 스타일을 적용할 문자열을 정의
+                            append("J")
+                        }
+//                        withStyle에서 설정한 스타일을 적용하지 않기를 원하면, 최상단에서 append
+//                        기본스타일, 즉 아래의 스타일들이 적용될 것이다.
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+//                            withStyle에서 정의한 스타일을 적용하기 위하여,
+//                            withStyle 내부의 블록에서 append를 통하여 해당 스타일을 적용할 문자열을 정의
+                            append("C")
+                        }
+                        append("ompose")
+                    },
+//                text styling
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Thin,
+                    fontStyle = FontStyle.Italic,
+//                    textAlign 속성 : 부모 요소 내부에서 정렬되는 방법 정의
+                    textAlign = TextAlign.Center,
+//                    textDecoration을 통하여 꾸밀 수 있음
+                    textDecoration = TextDecoration.Underline
+                )
+
             }
         }
     }
 }
 
-// Painter 자료형 : image resources를 사용(display)할 수 있도록 함
-// composable 함수에 modifier도 넘겨줄 수 있다. 이 과정에서 빈 modifier를 default로 넘겨줄 수도 있다.
+// Compose에서 Text를 스타일링 하는 방법
 /*
-여기에서 기본적으로 제공되는 Card Composable을 이용한다.
-=> XML에서 사용되는 Card 뷰와 유사
-modifier의 경우 함수의 인자로 넘겨받은 modifier를 이용한다.(Modifier가 아님)
-shape : 말 그대로 카드의 shape을 정의.
-elevation : 그림자
-여기서 만들고 싶은 것은 사진이 카드를 꽉 채우고, 사진 위에서 카드의 밑 부분에 설명글이 적혀있기를 원함.
-이 과정에서 column을 사용하는 것은 부적절할 것
-이 때, Box라는 Composable을 이용하는 것이다.
- */
+* Font를 사용하는 방법
+* 1. google fonts로 들어간다.(fonts.google.com)
+* 2. 특정 폰트를 선택한다.
+* 3. Download family 클릭하여 다운로드
+* 4. 글꼴파일을 프로젝트의 font 디렉토리에 붙여넣는다.
+*  => resource directory를 제작. 이름은 font로 한다.
+* 이 때 주의할 점은, 파일명이 모두 소문자로 되어있어야 하며, -는 _로 대체되어야 한다.
+* _ 이외의 모든 특수문자는 배제
+* android studio resource file명의 convention이기 때문
+*
+* *** annotatedString을 통하여, 텍스트의 특정 부분만의 스타일을 설정할 수도 있다.
+* 텍스트의 일부 글자만 스타일링 하는 것은 기존의 xml로도 가능은 했지만,
+* 매우 불편한 방식으로 해야했는데 Jetpack Compose에서는 이를 편리하게 가능
+* */
