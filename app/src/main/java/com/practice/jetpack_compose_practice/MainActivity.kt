01726510aec8ac578974080203b50f41d3883ef4
Kim Gimun <kmkim7575@gmail.com>
2023-05-29 13:32:02 +0900

    [FEAT] 버튼 클릭 색상 변화 구현

--- app/src/main/java/com/practice/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/com/practice/jetpack_compose_practice/MainActivity.kt
@@ -3,116 +3,84 @@ package com.practice.jetpack_compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.practice.jetpack_compose_practice.ui.theme.JetpackcomposepracticeTheme
-import com.practice.jetpack_compose_practice.ui.theme.Shapes
+import kotlin.random.Random
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        상황에 따라 다른 종류의 font를 사용하기 위하여 FontFamily를 이용
-        val fontFamily = FontFamily(
-//            fontweight가 thin으로 설정되었을 때 사용할 폰트
-            Font(R.font.playfair_italic_variablefont, FontWeight.Thin),
-            Font(R.font.playfair_variablefont, FontWeight.Light),
-//            ctrl+d를 하면 해당 코드를 붙여넣을 수 있음
-        )
-
         setContent {
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFF101010))) {
-                Text(
-//                    annotatedString을 통하여, 텍스트의 일부만 스타일링 가능
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp
-                            )
-                        ) {
-//                            withStyle에서 정의한 스타일을 적용하기 위하여,
-//                            withStyle 내부의 블록에서 append를 통하여 해당 스타일을 적용할 문자열을 정의
-                            append("J")
-                        }
-//                        withStyle에서 설정한 스타일을 적용하지 않기를 원하면, 최상단에서 append
-//                        기본스타일, 즉 아래의 스타일들이 적용될 것이다.
-                        append("etpack ")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp
-                            )
-                        ) {
-//                            withStyle에서 정의한 스타일을 적용하기 위하여,
-//                            withStyle 내부의 블록에서 append를 통하여 해당 스타일을 적용할 문자열을 정의
-                            append("C")
-                        }
-                        append("ompose")
-                    },
-//                text styling
-                    color = Color.White,
-                    fontSize = 30.sp,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Thin,
-                    fontStyle = FontStyle.Italic,
-//                    textAlign 속성 : 부모 요소 내부에서 정렬되는 방법 정의
-                    textAlign = TextAlign.Center,
-//                    textDecoration을 통하여 꾸밀 수 있음
-                    textDecoration = TextDecoration.Underline
-                )
 
+            Column(Modifier.fillMaxSize()) {
+//                자식 Composable에도 사용되는 state 변수를 정의 가능. external state
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+//            weight : Column/row 안에서 공간을 차지하는 비중
+                ColorBox(modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize()) {
+                    color.value = it
+                }
+
+                Box(modifier = Modifier
+//                  또다른 Box를 만들려고 하는데, 그런데 Colorbox에서 선언하였던 color state변수를 이용하고자 함.
+//
+                    .background(color.value)
+                    .weight(1f)
+                    .fillMaxSize()) {
+
+                }
             }
+
         }
     }
 }
 
-// Compose에서 Text를 스타일링 하는 방법
-/*
-* Font를 사용하는 방법
-* 1. google fonts로 들어간다.(fonts.google.com)
-* 2. 특정 폰트를 선택한다.
-* 3. Download family 클릭하여 다운로드
-* 4. 글꼴파일을 프로젝트의 font 디렉토리에 붙여넣는다.
-*  => resource directory를 제작. 이름은 font로 한다.
-* 이 때 주의할 점은, 파일명이 모두 소문자로 되어있어야 하며, -는 _로 대체되어야 한다.
-* _ 이외의 모든 특수문자는 배제
-* android studio resource file명의 convention이기 때문
-*
-* *** annotatedString을 통하여, 텍스트의 특정 부분만의 스타일을 설정할 수도 있다.
-* 텍스트의 일부 글자만 스타일링 하는 것은 기존의 xml로도 가능은 했지만,
-* 매우 불편한 방식으로 해야했는데 Jetpack Compose에서는 이를 편리하게 가능
-* */
+@Composable
+fun ColorBox(modifier: Modifier = Modifier, updateColor: (Color) -> Unit) {
+//    1. state 변수 선언하는 방법 : mutableStateOf()
+//    이 변수의 값이 변경되면, 이 변수가 속한 해당 Composable 함수는 처음부터 끝까지 다시 실행된다.
+//    따라서, composable 안에서 해당 변수를 정의하면 항상 노란색이 뜨므로 이는 적절하지 못한 방법이다.
+//    val color = mutableStateOf(Color.Yellow)
+//    이 때 사용할 수 있는 방법이 remember를 사용하는 것이다.
+//    remember : lambda function that comes from compose,
+//    it will just remember the value of the state from the last composition.
+//    이것을 통해, 재렌더링이 일어나도 color의 값은 Yellow로 초기화가 되지 않게 된다.
+//    val color = remember {
+//        mutableStateOf(Color.Yellow)
+//    }
+
+    Box(modifier = modifier
+//       2. state 변수의 value를 조회하는 방법 : state변수.value 키워드 이용
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+//            3. 클릭하였을 때, Box의 Background 색상이 변화하도록 하고 싶다면?
+//            이 때 state를 사용하면 된다. 상황에 따라 렌더링을 다르게 하고 싶다면 사용하는 것이 바로 state
+//            state 변수의 값을 '재할당'한다. 그러면 색상이 바뀌게 된다.(recomposing -> state change -> rerendering)
+//            mvvm에서 사용하는 livedata의 원리와 동일하다...
+//            랜덤 컬러를 설정하기 위하여, Random 클래스 이용
+
+        }) {
+
+
+    }
+}
