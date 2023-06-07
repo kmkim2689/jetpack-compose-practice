0db724db09e4fe61bc996e44ec8755ff28b2efac
Kim Gimun <kmkim7575@gmail.com>
2023-06-07 21:10:17 +0900

    [FEAT] constraint layout using constraintSet, Barriers

--- app/src/main/java/com/practice/jetpack_compose_practice/MainActivity.kt
+++ app/src/main/java/com/practice/jetpack_compose_practice/MainActivity.kt
@@ -1,98 +1,111 @@
 package com.practice.jetpack_compose_practice
 
-import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val scrollState = rememberScrollState()
-            // 방법1. Lazy Column을 사용하지 않고 반복되는 아이템에 대한 스크롤을 구현하는 방법
-            // 매우 비효율적
-//            Column(
-//                // Column에 이것을 작성해야 스크롤이 가능하다.
-//                // verticalscroll 인자들
-//                // 1. (scroll) state => 현재 scroll된 것에 관련한 state 관련 정보들을 담고 있음
-//                modifier = Modifier.verticalScroll(state = scrollState)
-//            ) {
-//                for (i in 1..50) {
-//                    Text(text = "Item $i", fontSize = 24.sp, fontWeight = FontWeight.Bold,
-//                        textAlign = TextAlign.Center,
-//                        modifier = Modifier
-//                            .fillMaxWidth()
-//                            .padding(vertical = 24.dp)
-//                    )
-//                }
-//            }
+            val constraints = ConstraintSet {
+                // in constriant set scope :
+                // to "define the constraints"
+                // create the references for each composable,
+                // to constrain in the layout
+                // 레이아웃에서 제약을 걸어줄 변수를 선언하고, createRefFor(id)를 이용하여
+                // 제약을 걸어주고 싶은 composable의 아이디를 여기서 설정한다. 문자열 형태로 넣는다.
+                // xml의 id 속성과 같다고 생각하면 됨. 즉 createRefFor는 뷰의 아이디를 설정하는 메소드
+                // 제약을 걸어주고 싶은 composable마다 해당 작업을 수행해야 한다.
+                val greenBox = createRefFor("greenbox")
+                val redBox = createRefFor("redbox")
 
-            // 방법2. Lazy Column
-            // 기본적으로 그 자체로 scroll이 가능하기 때문에, verticalscroll을 둘 필요가 없다.
-            // 아이템 하나에 대한 반복문을 두어 블록 안에 추가할 필요가 없이, 이미 이러한 기능을 해주는 함수를 이용
-            // 그런 함수가 바로 item(단일항목만 그리려고 할 때) 혹은 items(여러 개의 항목을 그려야 할 때, recyclerview에 대응)
-            // 하지만 itemsIndexed 함수가 더욱 효율적임
-            LazyColumn() {
-//                items 함수의 인자
-//                1. count : 얼마나 많은 개수를 넣을 것인지
+                // guidelines and barriers
+                val guideline = createGuidelineFromTop(0.5f) // 50% of the top
 
-//               itemsIndexed
-//               매개변수 : 리스트. 보여줄 데이터들에 대한 리스트. 데이터클래스 보통 사용함.
-                itemsIndexed(
-                    listOf("this", "is", "jt", "compose")
-                ) { index, string ->
-                    Text(text = "$string", fontSize = 24.sp, fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 24.dp)
-                    )
+                // 이렇게 제약을 걸어줄 변수들을 선언한 다음,
+                // constrain() 함수를 이용하여 제약조건을 명시한다.
+                // 제약을 걸어줄 ref 변수들을 안에 이용함으로써, 실제로 제약조건을 명시
+                // 제약 조건은 중괄호 scope 안에 작성
+                constrain(greenBox) {
+                    // 만약 greenBox를 맨 위 왼쪽에 붙이고 싶다면?
+                    // top of the parent, start of the parent
+                    // 제약 위치를 명시하는 함수 : linkTo() 함수
+
+                    top.linkTo(guideline)  // guideline으로 제약조건을 설정하는 경우, 'top'이 가이드라인에 붙음...
+                    // 해석 : top(<=첫번째로 나오는 top. 즉 greenbox를 의미)
+                    // totop(<=두 번째로 나오는 top, 즉 어디에 붙일 것인지를 의미)
+                    // of parent(<= parent)
+                    start.linkTo(parent.start)
+
+                    // 너비와 높이도 설정 가능
+                    // constraint layout내부의 뷰의 width, height 설정 가능
+                    // 설정 : Dimension.단위
+                    // ex : percent, value(dp 단위로 직접 설정), wrapContent, fillToConstraints(0dp와 같은 효과)
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
                 }
 
-//                items() 이용하면 미리 count를 정해놓아야 함. 비효율적
-//                items(5000) {
-////                    블록 안에는 아이템 안에 들어갈 요소들을 넣으면 됨.
-////                    여기서 it을 호출하면 그것은 인덱스를 나타냄.
-//                    Text(text = "Item $it", fontSize = 24.sp, fontWeight = FontWeight.Bold,
-//                        textAlign = TextAlign.Center,
-//                        modifier = Modifier
-//                        .fillMaxWidth()
-//                        .padding(vertical = 24.dp)
-//                    )
-//                }
+                constrain(redBox) {
+                    top.linkTo(parent.top)
+                    start.linkTo(greenBox.end)
+//                    width = Dimension.fillToConstraints
+                    width = Dimension.value(100.dp)
+                    height = Dimension.value(100.dp)
+                }
+
+                // 뷰끼리 chain을 형성하는 방법(서로가 서로에게 묶여있는 형태)
+                // create horizontal chain : endtostart, starttoend
+                // chain style : packed(중앙에 몰림), spread, spareadInside
+                //
+                createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Spread)
+
+                // 여기까지 작업만 수행하면, constraint set만 설정했지
+                // 아직 constraint layout이 정의되지 않았기 때문에
+                // 아무것도 보이지 않을 것임임            
             }
+            
+            // ConstraintLayout을 정의함으로써 constraintset 사용 가능
+            // 첫번째 인자로 constraint set 변수를 삽입
+            ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
+                // layoutId메소드를 통하여, constraintset에서 설정한 아이디와 같은 것을 찾아
+                // 제약조건을 해당 composable에 적용!!!
+                Box(modifier = Modifier
+                    .background(Color.Green)
+                    .layoutId("greenbox")) {
+
+                }
+
+                Box(modifier = Modifier
+                    .background(Color.Red)
+                    .layoutId("redbox")) {
+
+                }
+                
+            }
+
         }
     }
 }
+
 /*
-jetpack compose에서 recyclerview와 같은 기능을 구현하는 방법
-:LazyColumn 혹은 LazyRow를 사용하면 훨씬 간편하게 구현 가능
-LazyColumn : 여기서 Lazy라는 것은 Scroll되는 시점에서야 보여진다는 측면에서 사용되는 용어임.
-
-
-rememberScrollState는 Jetpack Compose에서 스크롤의 상태를 기억하고 관리하기 위해 사용되는 함수입니다. 스크롤 가능한 컴포넌트(예: LazyColumn, LazyRow, ScrollableColumn, ScrollableRow 등)와 함께 사용됩니다.
-
-rememberScrollState 함수는 스크롤의 현재 위치, 스크롤의 최소값 및 최대값, 스크롤 가능한 영역 등의 정보를 포함하는 ScrollState 객체를 생성합니다. 이 ScrollState 객체를 사용하여 스크롤을 제어하고 상태를 유지할 수 있습니다.
-
-주요 사용 사례는 다음과 같습니다:
-
-스크롤 위치 가져오기: scrollState.value를 통해 현재 스크롤 위치를 가져올 수 있습니다.
-스크롤 위치 설정: scrollState.scrollTo() 함수를 사용하여 스크롤 위치를 지정할 수 있습니다.
-스크롤 가능한 영역 제한: scrollState.minValue 및 scrollState.maxValue를 설정하여 스크롤 가능한 영역을 제한할 수 있습니다.
-스크롤 이벤트 처리: scrollState를 감시하고 변경에 따라 필요한 작업을 수행할 수 있습니다.
- */
+* constraint layout in jetpack compose
+constraint set을 만들고, 그것을 constraint layout에 할당
+row와 column을 nesting 하여 위치를 설정하여 사용하는 것보다는 이것을 사용하는 것이 훨씬 효율적일 것이다.
+*
+* Create references for each composable in the ConstraintLayout using the createRefs() or createRefFor()
+Constraints are provided using the constrainAs() modifier, which takes the reference as a parameter and lets you specify its constraints in the body lambda.
+Constraints are specified using linkTo() or other helpful methods.
+parent is an existing reference that can be used to specify constraints towards the ConstraintLayout composable itself.
+* */
